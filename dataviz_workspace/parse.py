397e2c994fa14ae180a2dc7940cab127ec5d2151
Raj Sumra <raj.sumratech@gmail.com>
2015-10-17 23:47:38 -0400

    Adding the parse.py file to Dataviz_workspace

---
+++ dataviz_workspace/parse.py
@@ -0,0 +1,16 @@
+import csv
+
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+ """Parses a raw CSV file to a JSON-line object. """
+
+    # Open CSV file
+
+    # Read CSV file
+
+    # Close CSV file
+
+    # Build a data structure to return parsed_data
+
+    return parsed_data
